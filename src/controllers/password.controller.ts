4f2a063b91702ad0d4ef71261913cc6edcbcacc0
lazynormz <skupi1234@gmail.com>
2023-11-04 16:59:40 +0100

    Created controller for adding passwords to database

---
+++ src/controllers/password.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express"
+import { Pool } from "mariadb"
+
+const RegisterNewPassword = (req: Request, res: Response, pool: Pool) => {
+
+}
+
+export {
+    RegisterNewPassword
+}
